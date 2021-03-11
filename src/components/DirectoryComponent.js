f7c2e36aa90bd8fab1e3e6168f79af6761126081
Kevin Kennedy <kkennedyy1@gmail.com>
2021-03-10 19:00:03 -0600

    React Router Parameters

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-function RenderDirectoryItem({campsite}) {
+function RenderDirectoryItem({ campsite }) {
     return (
         <Card>
-        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-        <CardImgOverlay>
-            <CardTitle>{campsite.name}</CardTitle>
-        </CardImgOverlay>
-    </Card>
+            <Link to={`/directory/${campsite.id}`}>
+                <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                <CardImgOverlay>
+                    <CardTitle>{campsite.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
+        </Card>
     );
 }
 
@@ -26,7 +29,7 @@ function Directory(props) {
         <div className="container">
             <div className="row">
                 {directory}
-            </div> 
+            </div>
         </div>
     );
 }
